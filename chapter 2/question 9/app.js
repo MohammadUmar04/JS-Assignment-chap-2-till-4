8e3247fd0993adb4b8e2c2cb86e6c5da8d91961b
MohammadUmar04 <muhammadomar009290@gmail.com>
2023-10-17 12:56:38 +0500

    question 9

---
+++ chapter 2/question 9/app.js
@@ -0,0 +1,20 @@
+// var name = prompt ("what is your Name?");
+// var age = prompt ("what is your Age?");
+// var school = prompt ("what is your school/ college /univwrsity Name ?");
+// var classes = prompt ("what is your field in your institue?");
+
+var name = "Umar";
+var age = 17;
+var school = "DJ College";
+var classes = "ICS- First Year";
+
+var nameStatement = "My name is " + name + ".";
+var ageStatement = "I am " + age + " years old.";
+var schoolStatement = "I study at " + school + ".";
+var classesStatement = "I take " + classes + " classes.";
+
+// Show the details in alert boxes
+alert(nameStatement);
+alert(ageStatement);
+alert(schoolStatement);
+alert(classesStatement);
