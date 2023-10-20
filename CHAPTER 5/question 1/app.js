aaad2e821287eb02b255e3f18fd61a24a4866ff3
MohammadUmar04 <muhammadomar009290@gmail.com>
2023-10-20 12:54:45 +0500

    Q5

---
+++ CHAPTER 5/question 1/app.js
@@ -0,0 +1,5 @@
+let num1 = 17;
+let num2 = 20;
+var result = num1 + num2;
+ document.write( " THE SUM OF " + num1 + " and " + num2 + " is " + result) 
+
---
+++ CHAPTER 5/question 2/app.js
@@ -0,0 +1,26 @@
+
+// question 2
+// Repeat task1 for subtraction, multiplication, division &
+// modulus.
+// subtraction.............
+let num1 = 45;
+let num2 = 9;
+let result = num1 - num2;
+document.write("The subtraction of " + num1 + " and " + num2 + " is " +result );
+
+// multiplication..............
+let a = 5;
+let b = 3;
+let ans = a * b;
+document.write("<br> The multiplication of " + a + " and " + b + " is " +ans );
+
+// division..............
+let c = 5;
+let d = 3;
+let output = c / d;
+document.write("<br> The division of " + c + " and " + d + " is " +output );
+// modulus..............
+let e = 5;
+let f = 3;
+let output2 = e % f;
+document.write("<br> The modulus of " + e + " and " + f + " is " +output2 )
