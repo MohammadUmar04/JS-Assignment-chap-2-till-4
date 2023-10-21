7647dc6f5d861056ae49d519c9217746ab2bdbe9
MohammadUmar04 <muhammadomar009290@gmail.com>
2023-10-21 22:12:21 +0500

    con

---
+++ CHAPTER 5/question 3/app.js
@@ -0,0 +1,18 @@
+// /question 3
+ let num = 5;
+ document.write("initial value:" + num ) 
+ 
+ num++;
+ document.write("<br> The value after increment is" + num);
+
+ num--;
+ document.write("<br> The value after decrement is" + num);
+
+ let remainder = num % 3;
+document.write("<br>The remainder is : " + remainder);
+
+let division = num/2;
+document.write("<br> the value after division is " + division);
+
+
+ 
---
+++ CHAPTER 5/question 4/app.js
@@ -0,0 +1,4 @@
+// question no 4 ;
+var total_tickets= prompt( "how many tickets do you want?")
+var ticket = 600;
+document.write( 'the price of the tickets are '+ticket*total_tickets);
